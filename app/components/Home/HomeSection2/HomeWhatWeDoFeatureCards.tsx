bf643fda52c47f1a1e564ba67c4c1bca8a98f8cc
AroraKaran19 <karora0319@gmail.com>
2024-10-17 02:55:12 +0530

    home section 2 completed

--- app/components/Home/HomeSection2/HomeWhatWeDoFeatureCards.tsx
+++ app/components/Home/HomeSection2/HomeWhatWeDoFeatureCards.tsx
@@ -4,8 +4,10 @@ const HomeWhatWeDoServiceCards = () => {
   return (
     <>
       <div className="feature-card-1 feature-card flex-1 flex min-h-[43.188rem] bg-[#8941FF] rounded-[3rem] pt-[4.25rem] shadow-2xl hover:mt-[4.25rem] hover:pt-0 hover:min-h-[38.938rem] transition-all duration-500 ease-in-out sm:min-h-0 sm:w-full sm:hover:mt-0 sm:hover:pt-[4.25rem] sm:hover:min-h-0">
-        <div className="feature-card-1-inner h-full w-full rounded-[2.8rem] bg-white shadow-lg hover:shadow-[#8941FF] transition-all duration-[500ms] ease-in-out border-[0.5px] border-black/40 overflow-hidden">
-          <div className="feature-container h-full w-full gap-[2.375rem] flex flex-col transition-all duration-[300ms] ease-in-out transform translate-y-[40%] sm:translate-y-0 px-[3.25rem] sm:py-5">
+        <div className="feature-card-1-inner h-full w-full rounded-[2.8rem] bg-white shadow-lg hover:shadow-[#8941FF] transition-all duration-[500ms] ease-in-out border-[0.5px] border-black/40 overflow-hidden relative">
+          <div className="feature-number absolute text-[41rem] text-[#8941FF] text-opacity-10 leading-none h-fit w-fit top-[-5%] right-5 z-40">1</div>
+          <div className="number-blur absolute top-2/3 bg-white/40 w-full z-[55] h-full backdrop-blur-[1px]"></div>
+          <div className="feature-container h-full w-full gap-[2.375rem] flex flex-col transition-all duration-[300ms] ease-in-out transform translate-y-[40%] sm:translate-y-0 px-[3.25rem] sm:py-5 z-[60] relative">
             <div className="feature-logo bg-[#F1E9FF] p-5 rounded-full w-fit">
               <img
                 src="/feature-github.png"
@@ -23,8 +25,10 @@ const HomeWhatWeDoServiceCards = () => {
         </div>
       </div>
       <div className="feature-card-2 feature-card flex-1 flex min-h-[43.188rem] bg-[#4C9FFF] rounded-[3rem] pt-[4.25rem] shadow-2xl hover:mt-[4.25rem] hover:pt-0 hover:min-h-[38.938rem] transition-all duration-500 ease-in-out sm:min-h-0 sm:w-full sm:hover:mt-0 sm:hover:pt-[4.25rem] sm:hover:min-h-0">
-        <div className="feature-card-2-inner h-full w-full rounded-[2.8rem] bg-white shadow-lg hover:shadow-[#4C9FFF] transition-all duration-[500ms] ease-in-out border-[0.5px] border-black/40 overflow-hidden">
-          <div className="feature-container h-full w-full gap-[2.375rem] flex flex-col transition-all duration-[300ms] ease-in-out transform translate-y-[40%] sm:translate-y-0 px-[3.25rem] sm:py-5">
+        <div className="feature-card-2-inner h-full w-full rounded-[2.8rem] bg-white shadow-lg hover:shadow-[#4C9FFF] transition-all duration-[500ms] ease-in-out border-[0.5px] border-black/40 overflow-hidden relative">
+        <div className="feature-number absolute text-[41rem] text-[#4C9FFF] text-opacity-10 leading-none h-fit w-fit top-[-5%] right-5 z-40">2</div>
+        <div className="number-blur absolute top-2/3 bg-white/40 w-full z-[55] h-full backdrop-blur-[1px]"></div>
+          <div className="feature-container h-full w-full gap-[2.375rem] flex flex-col transition-all duration-[300ms] ease-in-out transform translate-y-[40%] sm:translate-y-0 px-[3.25rem] z-[60] sm:py-5 relative">
             <div className="feature-logo bg-[#E2F0FF] p-5 rounded-full w-fit">
               <img src="/feature-resume.png" alt="Resume Icon" />
             </div>
@@ -38,8 +42,10 @@ const HomeWhatWeDoServiceCards = () => {
         </div>
       </div>
       <div className="feature-card-3 feature-card flex-1 flex min-h-[43.188rem] bg-[#FF783F] rounded-[3rem] pt-[4.25rem] shadow-2xl hover:mt-[4.25rem] hover:pt-0 hover:min-h-[38.938rem] transition-all duration-500 ease-in-out sm:min-h-0 sm:w-full sm:hover:mt-0 sm:hover:pt-[4.25rem] sm:hover:min-h-0">
-        <div className="feature-card-3-inner h-full w-full rounded-[2.8rem] bg-white shadow-lg hover:shadow-[#FF783F] transition-all duration-[500ms] ease-in-out border-[0.5px] border-black/40 overflow-hidden">
-          <div className="feature-container h-full w-full gap-[2.375rem] flex flex-col transition-all duration-[300ms] ease-in-out transform translate-y-[40%] sm:translate-y-0 px-[3.25rem] sm:py-5">
+        <div className="feature-card-3-inner h-full w-full rounded-[2.8rem] bg-white shadow-lg hover:shadow-[#FF783F] transition-all duration-[500ms] ease-in-out border-[0.5px] border-black/40 overflow-hidden relative">
+        <div className="feature-number absolute text-[41rem] text-[#FF783F] text-opacity-10 leading-none h-fit w-fit top-[-5%] right-5 z-40">3</div>
+        <div className="number-blur absolute top-2/3 bg-white/40 w-full z-[55] h-full backdrop-blur-[1px]"></div>
+          <div className="feature-container h-full w-full gap-[2.375rem] flex flex-col transition-all duration-[300ms] ease-in-out transform translate-y-[40%] sm:translate-y-0 px-[3.25rem] z-[60] sm:py-5 relative">
             <div className="feature-logo bg-[#FDE4D9] p-5 rounded-full w-fit">
               <img src="/feature-roadmap.png" alt="Roadmap Icon" />
             </div>
