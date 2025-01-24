d9e81ad861fb1c2aa5234954cc88aafea7a8ddcf
AroraKaran19 <karora0319@gmail.com>
2025-01-24 06:57:53 +0530

    (Changes) Updated the revamped website link!

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://seniorsage.vercel.app",
+  siteUrl: process.env.SITE_URL || "https://seniorsage-revamped.vercel.app/",
   generateRobotsTxt: true,
 };
